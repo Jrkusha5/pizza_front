17dab9a895c1f8b7232dfaad98d9f49499bb2a5d
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2024-10-11 02:48:52 -0700

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from "react-redux";
 import store from './redux/store.js';
-// import { AdminProvider } from '../context/AdminContext.js';
+;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      {/* <AdminProvider> */}
+      
         <App />
-      {/* </AdminProvider> */}
+      
     </Provider>
   </StrictMode>
 );
